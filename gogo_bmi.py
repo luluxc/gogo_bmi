c4ba07047147565f3f6195e2da3b26dd02dc23f6
luluxc <133820078+luluxc@users.noreply.github.com>
2023-05-21 20:13:18 -0500

    Update gogo_bmi.py

--- gogo_bmi.py
+++ gogo_bmi.py
@@ -90,6 +90,9 @@ def prepare_download(img):
     image_bytes = buf.getvalue()
     return image_bytes
 
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 # @st.cache_data
 # def decodeImage(bytesIo):
 #     fmt = whatimage.identify_image(bytesIo)
@@ -138,7 +141,7 @@ def main():
   webrtc_streamer(key="example",video_transformer_factory=VideoProcessor,rtc_configuration={'iceServers': get_ice_servers()},sendback_audio=False)
   
   col2, col3 = st.columns([2,1])
-  upload_img = col3.file_uploader('Upload a photo 🖼', on_change=change_photo_state)
+  upload_img = col3.file_uploader('Upload a photo 🖼', type=['png', 'jpeg', 'jpg'], on_change=change_photo_state)
   file_image = col2.camera_input('Take a pic of you 😊', on_change=change_photo_state)         
 
   if st.session_state['photo'] == 'Done':
@@ -170,25 +173,28 @@ def main():
       for process in range(100):
         time.sleep(0.01)
         process_bar3.progress(process+1)
-      col3.success('Uploaded the photo sucessfully!')
+      if allowed_file(upload_img.filename):
+        col3.success('Uploaded the photo sucessfully!')
 #       new_img = decodeImage(upload_img.getvalue())
 #       if new_img:
 #          upload_img = Image.open(new_img)
-      upload_img = Image.open(upload_img)
-      upload_img = np.array(upload_img.convert('RGB'))
-      pred_upload = predict_bmi(upload_img)[0]
-      if len(pred_upload) == 0:
-        col2.warning('No face detected, please upload a photo again.')
-      ready_upload = Image.fromarray(upload_img)
-      col2.image(ready_upload, clamp=True)
-      # Convert the PIL Image to bytes
-      download_img = prepare_download(ready_upload)
-      col3.write('Download the predicted image if you want!')
-      download_img = col3.download_button(
-        label='Download image', 
-        data=download_img,
-        file_name='BMI_image_uploaded.png',
-        mime="image/png")
+        upload_img = Image.open(upload_img)
+        upload_img = np.array(upload_img.convert('RGB'))
+        pred_upload = predict_bmi(upload_img)[0]
+        if len(pred_upload) == 0:
+          col2.warning('No face detected, please upload a photo again.')
+        ready_upload = Image.fromarray(upload_img)
+        col2.image(ready_upload, clamp=True)
+        # Convert the PIL Image to bytes
+        download_img = prepare_download(ready_upload)
+        col3.write('Download the predicted image if you want!')
+        download_img = col3.download_button(
+          label='Download image', 
+          data=download_img,
+          file_name='BMI_image_uploaded.png',
+          mime="image/png")
+      else:
+        col2.warning('Please upload an image in {jpg,jepg,png} type.')
   
   expander_col2 = col2.expander('What are the health consequences of having a high or low BMI?')
   expander_col2.write('Obesity carries significant health hazards, whereas maintaining a healthy weight is a preventative measure against illnesses and cardiovascular difficulties. People with a BMI of more than 30 are more likely to have problems such as:')
